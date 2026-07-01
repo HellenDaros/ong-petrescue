e8fb772080e6f0ef414aa34e5587583ae5c9a350
Hellen Daros <hellendaros@hotmail.com>
2026-06-30 22:25:20 -0300

    ajuste swagger

--- backend/src/main/java/com/senac/backend/backend/presentation/EnderecoController.java
+++ backend/src/main/java/com/senac/backend/backend/presentation/EnderecoController.java
@@ -3,6 +3,8 @@ package com.senac.backend.backend.presentation;
 
 import com.senac.backend.backend.application.DTO.EnderecoResponse;
 import com.senac.backend.backend.application.services.EnderecoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/enderecos")
+@Tag(name = "Consulta ViaCep", description = "Serviço para consulta da api ViaCep")
 public class EnderecoController {
 
     private final EnderecoService enderecoService;
@@ -20,6 +23,7 @@ public class EnderecoController {
     }
 
     @GetMapping("/{cep}")
+    @Operation(summary = "Consulta do CEP", description = "Busca o Cep")
     public ResponseEntity<EnderecoResponse> buscarEndereco(@PathVariable String cep) {
         return ResponseEntity.ok(enderecoService.buscarEnderecoFormatado(cep));
     }
